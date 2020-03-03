969ab02eb47f74a3ee917ea3f7d0439fdd42f5e9
Jacob Sansbury <sansburyjacob@gmail.com>
2020-03-03 12:15:06 -0800

    Configure travis and appveyor
    Update appveyor crate name
    
    Change crate path
    
    Refactor director structure
    
    Don't panic without source file
    
    Add CI folder
    
    Building for iOS is great but not needed
    
    Remove unused builds from travis
    
    remove BSD builds
    
    Fix travis config
    
    Built for any commit
    
    Update before_deploy script
    
    Change access key
    
    Fix key
    
    Redueced permissions of API key
    
    tTest

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+mod file;
+mod interpreter;
+mod lexer;
+mod parser;
+mod types;
+
+use std::collections::HashMap;
+use types::symbol_store::SymbolStore;
+
+fn main() {
+    let root_dir = std::env::current_dir()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        let first_source_path = args[1].to_string();
+        let source_text = file::read_source_file(first_source_path, root_dir);
+        if let Ok(tokens) = lexer::tokenize(source_text) {
+            let ast = parser::build_program(tokens);
+            let symbols: SymbolStore = HashMap::new();
+            interpreter::interpret(ast, symbols);
+        }
+    } else {
+        println!("First argument must be a source file.")
+    }
+}
