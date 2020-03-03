0d354baf6b289799983fc38b32d40f9c1c662936
Jacob Sansbury <sansburyjacob@gmail.com>
2020-03-03 15:55:08 -0800

    Test lexer

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,2 +1,45 @@
 mod lexer;
 pub use self::lexer::tokenize;
+
+#[cfg(test)]
+mod tokens {
+    use super::*;
+    use crate::types::binary_operator::BinaryOperator;
+    use crate::types::token::Token;
+    #[test]
+    fn add_numbers() {
+        let program = "10 + 20 * 1;".to_string();
+        let tokens = lexer::tokenize(program).unwrap();
+        assert_eq!(
+            tokens,
+            vec![
+                Token::Number(10),
+                Token::Operator(BinaryOperator::Add),
+                Token::Number(20),
+                Token::Operator(BinaryOperator::Multiply),
+                Token::Number(1),
+                Token::Semicolon
+            ]
+        )
+    }
+
+    #[test]
+    fn def_proc() {
+        let program = "proc a() {\nreturn false;\n}".to_string();
+        let tokens = lexer::tokenize(program).unwrap();
+        assert_eq!(
+            tokens,
+            vec![
+                Token::Proc,
+                Token::Symbol("a".to_string()),
+                Token::LParen,
+                Token::RParen,
+                Token::LBrace,
+                Token::Return,
+                Token::Bool(false),
+                Token::Semicolon,
+                Token::RBrace
+            ]
+        )
+    }
+}
